ec44776b9036df8271c790f58a31c6c65bf45ab1
Jean Kevin <rasen414@gmail.com>
2019-05-06 16:33:54 +0200

    spring-boot architecture

---
+++ src/main/java/com/polytech/ekwalsharezapi/EkwalSharezApiApplication.java
@@ -0,0 +1,13 @@
+package com.polytech.ekwalsharezapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkwalSharezApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkwalSharezApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polytech/ekwalsharezapi/EkwalSharezApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.polytech.ekwalsharezapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EkwalSharezApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
