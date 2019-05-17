20c55b95fb65a363586f134978bd4ef45da3b205
Jean Kevin <rasen414@gmail.com>
2019-05-17 11:45:11 +0200

    change connection

--- src/main/java/com/polytech/ekwalsharezapi/controller/UserController.java
+++ src/main/java/com/polytech/ekwalsharezapi/controller/UserController.java
@@ -34,10 +34,8 @@ public class UserController {
     @ApiResponses(value = {//
             @ApiResponse(code = 400, message = "Something went wrong"), //
             @ApiResponse(code = 401, message = "Invalid username/password supplied")})
-    public String login(//
-                        @ApiParam("Username") @RequestParam String username, //
-                        @ApiParam("Password") @RequestParam String password) {
-        return userService.signin(username, password);
+    public String login(@RequestBody UserDataDTO user) {
+        return userService.signin(user.getEmail(), user.getPassword());
     }
 
 }
